11ed007eb713b6e333afcc29d60a5712314030ef
Alef Carlos <alef.carlos@gmail.com>
2018-01-17 23:24:12 -0200

    feat: azure configurations

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Listen for messages from users 
