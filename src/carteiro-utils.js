ce56f594131a17d7c1a6467cc11167811725ffc4
Alef Carlos <alef.carlos@gmail.com>
2018-01-25 00:06:20 -0200

    feat: melhorada exibição das informações de rastreio.

--- src/carteiro-utils.js
+++ src/carteiro-utils.js
@@ -40,14 +40,55 @@ const tracakingFinishedList = [
 let getTrackingIndex = (array, trackingCode) => array.findIndex((element) => element.trackingCode === trackingCode);
 
 /**
- * Valida se um rastreio foi finalizado
+ * Valida se um rastreio da lista de histórico foi finalizado
  * @param {*} trackingInfo Informações do rastreio
  * @returns {Boolean} Retorna true, se o item já está marcado como entregue ao usuário, senão false.
  */
 let trackingIsFinished = (trackingInfo) => tracakingFinishedList.some((seachElement) => parseInt(seachElement.status, 10) === parseInt(trackingInfo.lastStatus, 10) && seachElement.type === trackingInfo.lastType);
 
+/**
+ * Converte um retorndo da api de correios para a entidade para salvarmos no banco
+ * @param {*} trackingInfo Informações de rastreio
+ * @param {Boolean} [getDestination=true]  Indicar true se devemos obter a descrição de destino, por padrão true
+ * @returns {*} Retorna o obketo criado a partir das informações de rastreio
+ */
+let trackingInfoToHistory = (trackingInfo, getDestination) => {
+
+    if (getDestination === undefined)
+        getDestination = true;
+
+    const _lastEvent = trackingInfo.evento[0];
+
+    let _newHistory = {
+        trackingCode: trackingInfo.numero,
+        trackingCategory: trackingInfo.categoria,
+        lastType: _lastEvent.tipo,
+        lastStatus: _lastEvent.status,
+        lastDescription: _lastEvent.descricao,
+        trackingTime: new Date().toISOString(),
+        lastDestination: getDestination ? getTrackingDestination(trackingInfo) : ''
+    };
+
+    return _newHistory;
+};
+
+let getTrackingDestination = (info) => {
+    const _lastEvent = info.evento[0];
+
+    //Se o objeto já estiver entregue, devemos simplesmente ler a cidade e uf
+    if (trackingIsFinished(trackingInfoToHistory(info, false)))
+        return `${_lastEvent.cidade} - ${_lastEvent.uf}`;
+
+    //Objeto ainda não entregue, então devemos ler a informação da propriedade destino
+    const _destination = _lastEvent.destino;
+
+    return `[${_destination.local}] ${_destination.cidade} - ${_destination.uf}`;
+};
+
 module.exports = {
     tracakingFinishedList,
     getTrackingIndex,
-    trackingIsFinished
+    trackingIsFinished,
+    getTrackingDestination,
+    trackingInfoToHistory
 };
