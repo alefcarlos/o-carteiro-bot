e3367f70967fa825e45cd372d256e33e9fabdf03
alefcarlos <alef.carlos@gmail.com>
2018-01-26 15:38:02 -0200

    feat: adicionada opções nas instruções

--- app.js
+++ app.js
@@ -74,8 +74,17 @@ bot.dialog('/', intents);
 
 //Diálogo menu principal
 bot.dialog('instructions', function (session) {
-    session.send("Me diga o que você gostaria de fazer, por exemplo: 'rastrear meu item' ou 'ver histórico de pesquisa'. Você pode até me passar o código junto: rastrear AA100833276BR")
-    .endDialog();
+
+    const msg = new builder.Message(session)
+    .text(`${session.userData.userName}, diga o que você gostaria de fazer, por exemplo: 'rastrear meu item' ou 'ver histórico de pesquisa'. Você pode até me passar o código junto: rastrear AA100833276BR"`)
+    .suggestedActions(
+    builder.SuggestedActions.create(
+        session, [
+            builder.CardAction.imBack(session, "rastrear", "Rastrear"),
+            builder.CardAction.imBack(session, "ver histórico", "Ver histórico")
+        ]
+    ));
+    session.send(msg).endDialog();
 });
 
 //Diálogo que reconhece o usuário
