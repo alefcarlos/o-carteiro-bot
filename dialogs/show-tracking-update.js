c673e8437c9b7ce815258240f08696a813827900
Alef Carlos <alef.carlos@gmail.com>
2018-02-11 17:07:38 -0200

    feat: adicionado card de notificação de atualização

--- dialogs/show-tracking-update.js
+++ dialogs/show-tracking-update.js
@@ -1,9 +1,34 @@
 const builder = require('botbuilder');
 const carteiroAPI = require('../carteiro-api');
+const carteiroUtils = require('../carteiro-utils');
 
 //Exibe mensagem dizendo que o item já foi entregue
 module.exports = function (session, args) {
-    session.send("Nova notificação");
+    if (session.userData.trackingHistory == undefined)
+        return;
+
+    let _trackingUpdateIndex = carteiroUtils.getTrackingIndex(session.userData.trackingHistory, args.code);
+
+    if (_trackingUpdateIndex == -1)
+        return;
+
+    session.send("Uma nova atualização de status para você !");
+
+    let _entityToUpdate = session.userData.trackingHistory[_trackingUpdateIndex];
+
+    _entityToUpdate.lastType = args.type;
+    _entityToUpdate.lastStatus = args.status;
+    _entityToUpdate.lastDescription = args.description;
+    _entityToUpdate.trackingTime = new Date().toISOString();
+    _entityToUpdate.lastDestination = args.destination;
+
+    const msg = new builder.Message(session);
+    msg.addAttachment(new builder.HeroCard(session)
+        .title(_entityToUpdate.trackingCode)
+        .subtitle(`${_entityToUpdate.lastDescription}`)
+        .text(_entityToUpdate.lastDestination));
+
+    session.send(msg).endDialog();
 
     carteiroAPI.setSeen(args.id)
         .then(() => { })
