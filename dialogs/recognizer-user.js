012b2e1b93b036eed7a58e1d7db8bc062f45c319
Alef Carlos <alef.carlos@gmail.com>
2018-02-20 23:23:52 -0300

    feat: identificação do nome de usuário no facebook e skype

--- dialogs/recognizer-user.js
+++ dialogs/recognizer-user.js
@@ -1,6 +1,31 @@
 const builder = require('botbuilder');
 const carteiroAPI = require('../carteiro-api');
 
+/**
+ * Se o canal for facebook ou skype com usuario logado
+ * obtemos o nome a partir do canal
+ * @param {*} session Informações da sessão
+ * @returns {string} String vazio ou nome do usuário
+ */
+let geUserNameFromChannel = (session) => {
+    const available = ['facebook', 'skype'];
+    const channel = session.message.address.channelId;
+
+    const exist = available.findIndex((value) => {
+        return value == channel;
+    })
+
+    if (exist == -1)
+        return '';
+
+    const user = session.message.address.user;
+
+    if (user.name == undefined)
+        return '';
+
+    return user.name;
+}
+
 //Diálogo que reconhece o usuário
 module.exports = [
     function (session, args, next) {
@@ -17,10 +42,18 @@ module.exports = [
 
         if (_user) {
             session.replaceDialog('instructions');
+            return;
         }
-        else {
-            builder.Prompts.text(session, 'Opa, primeiro vamo nos conhecer. Qual seu nome ?');
+
+        _user = geUserNameFromChannel(session);
+
+        if (_user) {
+            session.userData.userName = _user;
+            session.userData.trackingHistory = [];
+            return;
         }
+
+        builder.Prompts.text(session, 'Opa, primeiro vamo nos conhecer. Qual seu nome ?');
     },
     function (session, results) {
         session.userData.userName = results.response.trim();
